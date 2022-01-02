a9fe2e0ec4ca189402c5a116d585682edbf2d3b4
0x <wanghao187@gmail.com>
2022-01-02 17:03:33 +0800

    添加日志输出

--- pallets/kitties/src/lib.rs
+++ pallets/kitties/src/lib.rs
@@ -30,8 +30,7 @@ pub mod pallet {
 	}
 
 	type AccountOf<T> = <T as frame_system::Config>::AccountId;
-	type BalanceOf<T> =
-		<<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+	type BalanceOf<T> = <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
 
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
@@ -51,161 +50,145 @@ pub mod pallet {
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// A new Kitty was sucessfully created.
 		Created(T::AccountId, T::KittyIndex),
-		/// Kitty price was sucessfully set and on sell.
-		OnSell(T::AccountId, T::KittyIndex, Option<BalanceOf<T>>),
-		/// A Kitty was sucessfully transferred.
+		OnSales(T::AccountId, T::KittyIndex, Option<BalanceOf<T>>),
 		Transferred(T::AccountId, T::AccountId, T::KittyIndex),
-		/// A Kitty was sucessfully bought.
 		Bought(T::AccountId, T::AccountId, T::KittyIndex, Option<BalanceOf<T>>),
 	}
 
+	/// 定义存储
 	#[pallet::storage]
 	#[pallet::getter(fn kitties_count)]
 	pub type KittiesCount<T: Config> = StorageValue<_, T::KittyIndex>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn owner)]
-	pub type Owner<T: Config> =
-		StorageMap<_, Blake2_128Concat, T::KittyIndex, Option<T::AccountId>, ValueQuery>;
+	pub type Owner<T: Config> = StorageMap<_, Blake2_128Concat, T::KittyIndex, Option<T::AccountId>, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn kitties)]
-	pub type Kitties<T: Config> =
-		StorageMap<_, Blake2_128Concat, T::KittyIndex, Option<Kitty<T>>, ValueQuery>;
+	pub type Kitties<T: Config> = StorageMap<_, Blake2_128Concat, T::KittyIndex, Option<Kitty<T>>, ValueQuery>;
 
 	#[pallet::storage]
 	#[pallet::getter(fn kitties_list_for_sale)]
-	pub type ListForSale<T: Config> =
-		StorageMap<_, Blake2_128Concat, T::KittyIndex, Option<BalanceOf<T>>, ValueQuery>;
+	pub type ListForSale<T: Config> = StorageMap<_, Blake2_128Concat, T::KittyIndex, Option<BalanceOf<T>>, ValueQuery>;
 
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Handles arithemtic overflow when incrementing the Kitty counter.
-		KittiesCountOverflow,
-		/// Buyer cannot be the owner.
+		KittyCntOverflow,
 		BuyerIsKittyOwner,
-		/// Handles checking whether the Kitty exists.
 		KittyNotExist,
-		/// Handles checking that the Kitty is owned by the account transferring, buying or setting a price for it.
 		NotKittyOwner,
-		/// Ensures the Kitty is on sale.
-		KittyNotOnSale,
-		/// Ensures that an account has enough funds to purchase a Kitty.
+		KittyNotForSale,
 		NotEnoughBalance,
-		/// Ensures has enough balance to stake.
 		NotEnoughBalanceForStaking,
 	}
 
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
+		/// 创建kitty
 		#[pallet::weight(0)]
 		pub fn create(origin: OriginFor<T>) -> DispatchResult {
 			let who = ensure_signed(origin)?;
 			let kitty_id = Self::mint(&who, None)?;
+			log::info!("创建了一个kitty,id: {:?}.", kitty_id);
 			Self::deposit_event(Event::Created(who, kitty_id));
-
 			Ok(())
 		}
 
+		/// 转移kitty
+		/// to: 转移到到的账户
+		/// kitty_id: 某个具体kitty的索引
 		#[pallet::weight(100)]
-		pub fn transfer(
-			origin: OriginFor<T>,
-			to: T::AccountId,
-			kitty_id: T::KittyIndex,
-		) -> DispatchResult {
+		pub fn transfer(origin: OriginFor<T>, to: T::AccountId, kitty_id: T::KittyIndex) -> DispatchResult {
 			let who = ensure_signed(origin)?;
+			// 校验是否kitty所有者
 			ensure!(Self::is_kitty_owner(&kitty_id, &who)?, <Error<T>>::NotKittyOwner);
+			// 转移kitty(内部包含kitty存在性验证)
 			Self::transfer_kitty_to(&kitty_id, &to)?;
+			log::info!("账户: {:?} 将id为 {:?} 的kitty, 从自己转移到 账户: {:?}.", who, kitty_id, to);
+			// 转移成功事件
 			Self::deposit_event(Event::Transferred(who, to, kitty_id));
-
 			Ok(())
 		}
 
+		/// 孵化kitty
+		/// father_kitty_id: 父kitty索引id
+		/// mother_kitty_id: 母kitty索引id
 		#[pallet::weight(100)]
-		pub fn breed(
-			origin: OriginFor<T>,
-			kitty_id_1: T::KittyIndex,
-			kitty_id_2: T::KittyIndex,
-		) -> DispatchResult {
+		pub fn breed(origin: OriginFor<T>, father_kitty_id: T::KittyIndex, mother_kitty_id: T::KittyIndex) -> DispatchResult {
 			let who = ensure_signed(origin)?;
-
-			let new_dna = Self::breed_dna(&who, kitty_id_1, kitty_id_2)?;
+			// 生成孵化dna
+			let new_dna = Self::breed_dna(&who, father_kitty_id, mother_kitty_id)?;
+			// 根据所有者和孵化dna铸造kitty
 			let kitty_id = Self::mint(&who, Some(new_dna))?;
-
+			log::info!("账户: {:?} 通过id为 {:?} 的kitty和id为 {:?} 的kitty,孵化出id为 {:?} 的kitty.", who, father_kitty_id, mother_kitty_id, kitty_id);
+			// 创建成功事件
 			Self::deposit_event(Event::Created(who, kitty_id));
-
 			Ok(())
 		}
 
-		#[pallet::weight(100)]
-		pub fn sell(
-			origin: OriginFor<T>,
-			kitty_id: T::KittyIndex,
-			price: Option<BalanceOf<T>>,
-		) -> DispatchResult {
-			let sender = ensure_signed(origin)?;
-			if !<Kitties<T>>::contains_key(&kitty_id) {
-				Err(<Error<T>>::KittyNotExist)?;
-			}
-			ensure!(Self::is_kitty_owner(&kitty_id, &sender)?, <Error<T>>::NotKittyOwner);
-
-			// Set price before sell.
-			ListForSale::<T>::try_mutate(kitty_id, |p| -> DispatchResult {
-				*p = price;
-				Ok(().into())
-			})?;
-
-			Self::deposit_event(Event::OnSell(sender, kitty_id, price));
-
-			Ok(())
-		}
-
+		
+		/// 购买kitty
+		/// kitty_id: 某个具体kitty的索引
 		#[transactional]
 		#[pallet::weight(100)]
-		pub fn buy_kitty(
-			origin: OriginFor<T>,
-			kitty_id: T::KittyIndex,
-			// bid_price: BalanceOf<T>,
-		) -> DispatchResult {
+		pub fn buy(origin: OriginFor<T>, kitty_id: T::KittyIndex) -> DispatchResult {
 			let buyer = ensure_signed(origin)?;
 
-			// Check the kitty exists and buyer is not the current kitty owner.
+			// 校验是否已经拥有kitty,不能自己买自己的
 			let kitty = Self::kitties(&kitty_id).ok_or(<Error<T>>::KittyNotExist)?;
 			ensure!(kitty.owner != buyer, <Error<T>>::BuyerIsKittyOwner);
 
-			// Check the buyer has enough free balance.
-			let ask_price =
-				Self::kitties_list_for_sale(&kitty_id).ok_or(<Error<T>>::KittyNotOnSale)?;
+			// 根据售卖和质押要求校验购买账号
+			let ask_price = Self::kitties_list_for_sale(&kitty_id).ok_or(<Error<T>>::KittyNotForSale)?;
 			let stake = T::StakeAmountForKitty::get();
 			let free_balance = T::Currency::free_balance(&buyer);
 			ensure!(free_balance > (ask_price + stake), <Error<T>>::NotEnoughBalance);
 
-			// Transfer the amount from buyer to seller.
+			// 转移资产
 			let seller = kitty.owner.clone();
 			T::Currency::transfer(&buyer, &seller, ask_price, ExistenceRequirement::KeepAlive)?;
 
-			// Transfer the kitty from seller to buyer.
+			// 转移kitty
 			Self::transfer_kitty_to(&kitty_id, &buyer)?;
 
-			// Remove kitty from sale list.
+			// 从售卖列表中移除该kitty
 			ListForSale::<T>::remove(kitty_id);
+			log::info!("账户: {:?} 花费 {:?} 从 账户: {:?} 购买了id为 {:?} 的kitty.", buyer, ask_price, seller, kitty_id);
 
 			Self::deposit_event(Event::Bought(buyer, seller, kitty_id, Some(ask_price)));
 
 			Ok(())
 		}
+
+		/// 上架销售
+		/// kitty_id: 某个具体kitty的索引
+		/// price； 上架销售价格
+		#[pallet::weight(100)]
+		pub fn sale(origin: OriginFor<T>, kitty_id: T::KittyIndex, price: Option<BalanceOf<T>>) -> DispatchResult {
+			let sender = ensure_signed(origin)?;
+			// kitty必须存在
+			ensure!(<Kitties<T>>::contains_key(&kitty_id), <Error<T>>::KittyNotExist);
+			// 交易所有者权限
+			ensure!(Self::is_kitty_owner(&kitty_id, &sender)?, <Error<T>>::NotKittyOwner);
+
+			// 设置价格并上架
+			ListForSale::<T>::try_mutate(kitty_id, |p| -> DispatchResult {
+				*p = price;
+				Ok(().into())
+			})?;
+
+			log::info!("账户: {:?} 将id为 {:?} 的kitty上架销售，销售价格为 {:?} .", sender, kitty_id, price);
+			Self::deposit_event(Event::OnSales(sender, kitty_id, price));
+
+			Ok(())
+		}
+
 	}
 
 	impl<T: Config> Pallet<T> {
-		fn is_kitty_owner(kitty_id: &T::KittyIndex, acct: &T::AccountId) -> Result<bool, Error<T>> {
-			match Self::kitties(kitty_id) {
-				Some(kitty) => Ok(kitty.owner == *acct),
-				None => Err(<Error<T>>::KittyNotExist),
-			}
-		}
-
+		/// 随机值
 		fn random_value(sender: &T::AccountId) -> [u8; 16] {
 			let payload = (
 				T::Randomness::random_seed(),
@@ -215,10 +198,22 @@ pub mod pallet {
 			payload.using_encoded(blake2_128)
 		}
 
-		pub fn mint(
-			owner: &T::AccountId,
-			dna: Option<[u8; 16]>,
-		) -> Result<T::KittyIndex, Error<T>> {
+		/// kitty所有者身份校验
+		/// kitty_id: kitty 索引id
+		/// account_id: 账户id
+		fn is_kitty_owner(kitty_id: &T::KittyIndex, account_id: &T::AccountId) -> Result<bool, Error<T>> {
+			match Self::kitties(kitty_id) {
+				Some(kitty) => Ok(kitty.owner == *account_id),
+				None => Err(<Error<T>>::KittyNotExist),
+			}
+		}
+
+		
+		/// 铸造
+		/// owner: 铸造kitty的所有者
+		/// dna: 铸造kitty的dna属性
+		pub fn mint(owner: &T::AccountId, dna: Option<[u8; 16]>) -> Result<T::KittyIndex, Error<T>> {
+			// 生成dna,并构建kitty
 			let dna_inner: [u8; 16];
 			if let Some(v) = dna {
 				dna_inner = v;
@@ -227,63 +222,55 @@ pub mod pallet {
 			}
 			let kitty = Kitty::<T> { dna: dna_inner, owner: owner.clone() };
 
+			// 构造kitty索引id,第一次索引为1，其余索引为
 			let kitty_id = match Self::kitties_count() {
 				Some(id) => {
-					ensure!(id != T::KittyIndex::max_value(), Error::<T>::KittiesCountOverflow);
-					id
+					ensure!(id != T::KittyIndex::max_value(), Error::<T>::KittyCntOverflow);
+					id.checked_add(&T::KittyIndex::one()).unwrap()
 				}
-				None => T::KittyIndex::min_value(),
+				None => T::KittyIndex::one()
 			};
 
+			// 质押
 			let stake = T::StakeAmountForKitty::get();
-			T::Currency::reserve(&owner, stake)
-				.map_err(|_| Error::<T>::NotEnoughBalanceForStaking)?;
+			T::Currency::reserve(&owner, stake).map_err(|_| Error::<T>::NotEnoughBalanceForStaking)?;
 
+			// 存储
 			Kitties::<T>::insert(kitty_id, Some(kitty));
 			Owner::<T>::insert(kitty_id, Some(owner));
-
-			let one = T::KittyIndex::one();
-			let next_kitty_index = kitty_id
-				.checked_add(&one)
-				.ok_or(one)
-				.map_err(|_| <Error<T>>::KittiesCountOverflow)?;
-			KittiesCount::<T>::put(next_kitty_index);
+			KittiesCount::<T>::put(kitty_id);
 
 			Ok(kitty_id)
 		}
 
+		/// 转移kitty(成功不返回;失败返回错误信息)
+		/// kitty_id: kitty索引id
+		/// to: 转移kitty到目标账户的accountId
 		#[transactional]
-		pub fn transfer_kitty_to(
-			kitty_id: &T::KittyIndex,
-			to: &T::AccountId,
-		) -> Result<(), Error<T>> {
+		pub fn transfer_kitty_to(kitty_id: &T::KittyIndex,to: &T::AccountId) -> Result<(), Error<T>> {
+			// 校验kitty是否存在
 			let mut kitty = Self::kitties(&kitty_id).ok_or(<Error<T>>::KittyNotExist)?;
-			let pre_owner = kitty.owner;
 
-			// Update the kitty owner.
-			kitty.owner = to.clone();
-
-			// Staking from new owner and unstaking from the pre ownder
+			// 质押
 			let stake = T::StakeAmountForKitty::get();
 			T::Currency::reserve(&to, stake).map_err(|_| Error::<T>::NotEnoughBalanceForStaking)?;
-			T::Currency::unreserve(&pre_owner, stake);
+			T::Currency::unreserve(&kitty.owner, stake);
 
-			// Inert kitty.
+			// 修改kitty所有者并存储
+			kitty.owner = to.clone();
 			Kitties::<T>::insert(kitty_id, Some(kitty));
-
-			// Insert new owner.
 			Owner::<T>::insert(kitty_id, Some(to.clone()));
 
 			Ok(())
 		}
 
-		fn breed_dna(
-			who: &T::AccountId,
-			parent1: T::KittyIndex,
-			parent2: T::KittyIndex,
-		) -> Result<[u8; 16], Error<T>> {
-			let dna1 = Self::kitties(parent1).ok_or(<Error<T>>::KittyNotExist)?.dna;
-			let dna2 = Self::kitties(parent2).ok_or(<Error<T>>::KittyNotExist)?.dna;
+		/// 构建dna(成功返回dna;失败返回错误信息)
+		/// who: 构建人accountId
+		/// father_kitty_id: 父kitty索引id
+		/// mother_kitty_id: 母kitty索引id
+		fn breed_dna(who: &T::AccountId, father_kitty_id: T::KittyIndex, mother_kitty_id: T::KittyIndex) -> Result<[u8; 16], Error<T>> {
+			let dna1 = Self::kitties(father_kitty_id).ok_or(<Error<T>>::KittyNotExist)?.dna;
+			let dna2 = Self::kitties(mother_kitty_id).ok_or(<Error<T>>::KittyNotExist)?.dna;
 
 			let selector = Self::random_value(&who);
 			let mut new_dna = [0u8; 16];
--- pallets/kitties/src/tests.rs
+++ pallets/kitties/src/tests.rs
@@ -7,8 +7,8 @@ fn test_create() {
 	new_test_ext().execute_with(|| {
 		assert_ok!(KittiesModule::create(Origin::signed(1)));
 		assert_eq!(KittiesCount::<Test>::get(), Some(1));
-		assert_eq!(Owner::<Test>::get(0), Some(1));
-		System::assert_has_event(MockEvent::KittiesModule(Event::Created(1, 0)));
+		assert_eq!(Owner::<Test>::get(1), Some(1));
+		System::assert_has_event(MockEvent::KittiesModule(Event::Created(1, 1)));
 	});
 }
 
@@ -16,9 +16,9 @@ fn test_create() {
 fn test_transfer() {
 	new_test_ext().execute_with(|| {
 		assert_ok!(KittiesModule::create(Origin::signed(1)));
-		assert_ok!(KittiesModule::transfer(Origin::signed(1), 2, 0));
-		assert_eq!(Owner::<Test>::get(0), Some(2));
-		System::assert_has_event(MockEvent::KittiesModule(Event::Transferred(1, 2, 0)));
+		assert_ok!(KittiesModule::transfer(Origin::signed(1), 2, 1));
+		assert_eq!(Owner::<Test>::get(1), Some(2));
+		System::assert_has_event(MockEvent::KittiesModule(Event::Transferred(1, 2, 1)));
 	});
 }
 
@@ -27,9 +27,9 @@ fn test_breed() {
 	new_test_ext().execute_with(|| {
 		assert_ok!(KittiesModule::create(Origin::signed(1)));
 		assert_ok!(KittiesModule::create(Origin::signed(2)));
-		assert_ok!(KittiesModule::breed(Origin::signed(1), 0, 1));
+		assert_ok!(KittiesModule::breed(Origin::signed(1), 1, 2));
 		assert_eq!(KittiesCount::<Test>::get(), Some(3));
-		System::assert_has_event(MockEvent::KittiesModule(Event::Created(1, 2)));
+		System::assert_has_event(MockEvent::KittiesModule(Event::Created(1, 3)));
 	});
 }
 
@@ -38,21 +38,22 @@ fn test_sell() {
 	new_test_ext().execute_with(|| {
 		assert_ok!(KittiesModule::create(Origin::signed(1)));
 		let price: u128 = 2000;
-		assert_ok!(KittiesModule::sell(Origin::signed(1), 0, Some(price)));
-		assert_eq!(ListForSale::<Test>::get(0), Some(price));
-		System::assert_has_event(MockEvent::KittiesModule(Event::OnSell(1, 0, Some(price))));
+		assert_ok!(KittiesModule::sale(Origin::signed(1), 1, Some(price)));
+		assert_eq!(ListForSale::<Test>::get(1), Some(price));
+		System::assert_has_event(MockEvent::KittiesModule(Event::OnSales(1, 1, Some(price))));
 	});
 }
 
 #[test]
 fn test_buy() {
 	new_test_ext().execute_with(|| {
+		assert_eq!(KittiesCount::<Test>::get(),None);
 		assert_ok!(KittiesModule::create(Origin::signed(1)));
 		let price: u128 = 1_500;
-		assert_ok!(KittiesModule::sell(Origin::signed(1), 0, Some(price)));
-		assert_ok!(KittiesModule::buy_kitty(Origin::signed(2), 0));
-		assert_eq!(Owner::<Test>::get(0), Some(2));
-		System::assert_has_event(MockEvent::KittiesModule(Event::Bought(2, 1, 0, Some(price))));
+		assert_ok!(KittiesModule::sale(Origin::signed(1), 1, Some(price)));
+		assert_ok!(KittiesModule::buy(Origin::signed(2), 1));
+		assert_eq!(Owner::<Test>::get(1), Some(2));
+		System::assert_has_event(MockEvent::KittiesModule(Event::Bought(2, 1, 1, Some(price))));
 	});
 }
 
@@ -63,7 +64,7 @@ fn test_kitties_count_overflow() {
 		let account_id: u64 = 1;
 		assert_noop!(
 			KittiesModule::create(Origin::signed(account_id)),
-			Error::<Test>::KittiesCountOverflow
+			Error::<Test>::KittyCntOverflow
 		);
 	});
 }
@@ -73,9 +74,9 @@ fn test_buyer_should_not_owner() {
 	new_test_ext().execute_with(|| {
 		assert_ok!(KittiesModule::create(Origin::signed(1)));
 		let price: u128 = 1_500;
-		assert_ok!(KittiesModule::sell(Origin::signed(1), 0, Some(price)));
+		assert_ok!(KittiesModule::sale(Origin::signed(1), 1, Some(price)));
 		assert_noop!(
-			KittiesModule::buy_kitty(Origin::signed(1), 0),
+			KittiesModule::buy(Origin::signed(1), 1),
 			Error::<Test>::BuyerIsKittyOwner
 		);
 	});
@@ -86,7 +87,7 @@ fn test_kitty_not_exist() {
 	new_test_ext().execute_with(|| {
 		let price: u128 = 1_500;
 		assert_noop!(
-			KittiesModule::sell(Origin::signed(1), 0, Some(price)),
+			KittiesModule::sale(Origin::signed(1), 0, Some(price)),
 			Error::<Test>::KittyNotExist
 		);
 	});
@@ -97,7 +98,7 @@ fn test_not_kitty_owner() {
 	new_test_ext().execute_with(|| {
 		assert_ok!(KittiesModule::create(Origin::signed(1)));
 		assert_noop!(
-			KittiesModule::transfer(Origin::signed(2), 3, 0),
+			KittiesModule::transfer(Origin::signed(2), 3, 1),
 			Error::<Test>::NotKittyOwner
 		);
 	});
@@ -107,7 +108,7 @@ fn test_not_kitty_owner() {
 fn test_kitty_not_on_sale() {
 	new_test_ext().execute_with(|| {
 		assert_ok!(KittiesModule::create(Origin::signed(1)));
-		assert_noop!(KittiesModule::buy_kitty(Origin::signed(2), 0), Error::<Test>::KittyNotOnSale);
+		assert_noop!(KittiesModule::buy(Origin::signed(2), 1), Error::<Test>::KittyNotForSale);
 	});
 }
 
@@ -116,9 +117,9 @@ fn test_not_enough_balance() {
 	new_test_ext().execute_with(|| {
 		assert_ok!(KittiesModule::create(Origin::signed(1)));
 		let price: u128 = 7_500;
-		assert_ok!(KittiesModule::sell(Origin::signed(1), 0, Some(price)));
+		assert_ok!(KittiesModule::sale(Origin::signed(1), 1, Some(price)));
 		assert_noop!(
-			KittiesModule::buy_kitty(Origin::signed(3), 0),
+			KittiesModule::buy(Origin::signed(3), 1),
 			Error::<Test>::NotEnoughBalance
 		);
 	});
